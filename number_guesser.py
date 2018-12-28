3160bd76b1f923994af62ebae7773fcd898bb023
FirstServe <ninaad.nair@gmail.com>
2018-12-28 14:16:58 -0600

    Allowing the user to guess the number.

--- number_guesser.py
+++ number_guesser.py
@@ -3,14 +3,24 @@ import math, random
 def generate_number():
     '''Generates a random positive integer from 1 to 1000.'''
     num = random.randint(1, 1000)
-    print(num)
-    return
+    return num
 
-def guess_number():
+
+def guess_number(num):
     '''Allows the user to guess the generated number.'''
-    return
+    guess = int(input("What is your guess?"))
+    while guess != num:
+        if guess > num:
+            print("Your guess was larger than my number. Please guess again.")
+        elif guess < num:
+            print("Your guess was smaller than my number. Please guess again.")
+        else:
+            print("An error occurred while evaluating your guess. Please try again.")
+        guess = int(input("What is your guess?"))
 
 def check_input():
     return
 
-generate_number()
+
+num = generate_number
+print(num)
