25590370cb030de7de3e7fa23741ede71b7a622f
FirstServe <ninaad.nair@gmail.com>
2018-12-28 10:54:57 -0600

    adding number guesser code

---
+++ number_guesser.py
@@ -0,0 +1,15 @@
+import math, random
+
+def generate_number():
+    '''Generates a random positive integer from 1 to 1000.'''
+    num = random.randint(1, 1000)
+    print(num)
+    return
+
+def guess_number():
+    return
+
+def check_input():
+    return
+
+generate_number()
